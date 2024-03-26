4de0e41c26c1d9a63998bc68149463211f786446
Tushar Banik <b.toshar@iitg.ac.in>
2024-03-26 12:20:27 +0530

    change source branch

---
+++ Backend/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import meeting,ugSenator,pgSenator,facultyExecutivebodie,studentExecutivebodie,upcomingEvent, upcomingEventlead
+class meetingSerializer(serializers.ModelSerializer):
+   class Meta:
+      model= meeting
+      fields='__all__'
+
+class ugSenatorSerializer(serializers.ModelSerializer):
+   class Meta:
+      model= ugSenator
+      fields='__all__'
+
+class pgSenatorSerializer(serializers.ModelSerializer):
+   class Meta:
+      model= pgSenator
+      fields='__all__'
+
+class facultyExcecutiveSerializer(serializers.ModelSerializer):
+   class Meta:
+      model= facultyExecutivebodie
+      fields='__all__'
+
+class studentExcecutiveSerializer(serializers.ModelSerializer):
+   class Meta:
+      model= studentExecutivebodie
+      fields='__all__'
+
+class upComingSerializer(serializers.ModelSerializer):
+   class Meta:
+      model= upcomingEvent
+      fields='__all__'
+
+class upComingleadSerializer(serializers.ModelSerializer):
+   class Meta:
+      model= upcomingEventlead
+      fields='__all__'
